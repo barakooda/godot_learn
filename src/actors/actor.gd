20374b1d840ed852f499259f3c27ff1f1786981e
barak.koren <barak.koren@datagen.tech>
2023-06-14 20:03:54 +0300

    wip godot learn

--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 class_name Actor
 
-
+@export var gravity: = 20
 
 func _physics_process(delta)-> void:
-	velocity = Vector2(100,0)
+	
+	velocity.y+=delta*gravity
 	move_and_slide()
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	pass # Replace with function body.
+func _ready():
+	velocity = Vector2(100,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

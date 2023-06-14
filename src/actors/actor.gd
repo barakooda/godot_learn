463944b86a6435ddb6af8b6acbeaf705872cb8df
barak.koren <barak.koren@datagen.tech>
2023-06-14 19:01:10 +0300

    wip godot learn

--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
+
 class_name Actor
 
 
+
+func _physics_process(delta)-> void:
+	velocity = Vector2(100,0)
+	move_and_slide()
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
+#func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#func _process(delta):
 	pass
